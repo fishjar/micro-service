3c0b3c55b94cc73c928c0d74e38d0e6941bf556a
gabe <yugang2002@gmail.com>
2018-03-08 17:35:25 +0800

    wxapp client

--- client-wxapp/pages/index/index.js
+++ client-wxapp/pages/index/index.js
@@ -1,5 +1,6 @@
 //index.js
 const { promise, request } = require('../../utils/util')
+const { appid } = require('../../utils/config')
 //获取应用实例
 const app = getApp()
 
@@ -29,8 +30,84 @@ Page({
   },
   bindUserTap() {
     const { id } = this.data.userInfo;
-    request(`/users/${id}`).then(res=>{
+    request(`/users/${id}`).then(res => {
       //
+    })
+  },
+  bindPay() {
+    request(`/test`, {}, 'POST').then(res => {
+      //
+    })
+  },
+  login() {
+    app.login();
+  },
+  uploadFile(e) {
+    wx.chooseImage({
+      success: function (res) {
+        const tempFilePaths = res.tempFilePaths
+        const { token, token_expire } = wx.getStorageSync('auth') || {}
+        // obj.header = {
+        //   authentication: token,
+        // };
+        wx.uploadFile({
+          // url: 'http://192.168.0.103:9106/upload/ajax',
+          // url: 'http://192.168.43.172:9101/upload',
+          url: 'http://192.168.0.103:9101/upload',
+          filePath: tempFilePaths[0],
+          header: {
+            authentication: token,
+          },
+          name: 'file',
+          formData: {
+            title: 'test',
+            user_id: 1,
+            description: 'description description',
+            resize: 400,
+            thumb: 120
+          },
+          success: function (res) {
+            console.log(res)
+            console.log(JSON.parse(res.data))
+            // var data = res.data
+            //do something
+          }
+        })
+      }
+    })
+  },
+  wxPay() {
+    const { openid } = this.data.userInfo;
+    if (!openid) {
+      return
+    }
+    request(`/wxpay`, {
+      appid,
+      openid,
+      body: 'test',
+      total_fee: 1,
+    }, 'POST').then(res => {
+      wx.requestPayment({
+        'nonceStr': res.nonceStr,
+        'package': res.package,
+        'paySign': res.paySign,
+        'signType': res.signType,
+        'timeStamp': res.timeStamp,
+        'success': function (res) {
+          wx.showModal({
+            title: '支付成功!',
+            showCancel: false
+          })
+        },
+        'fail': function (res) {
+          wx.showModal({
+            title: '支付失败!',
+            content: res,
+            showCancel: false
+          })
+        }
+      });
+
     })
   },
 })
--- client-wxapp/utils/config.js
+++ client-wxapp/utils/config.js
@@ -1,5 +1,6 @@
 // const api_host = 'http://0.0.0.0:9101'
-const api_host = 'http://192.168.0.101:9101'
+const api_host = 'http://192.168.0.103:9101'
+// const api_host = 'http://192.168.43.172:9101'
 const appid = 'wx7aacccc73ccea206'
 
 module.exports = {
